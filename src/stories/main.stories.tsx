40d5116ebf4cbb3bde1a34714247bae49b280bf3
iamgabrielsoft <iamgabrielsoft@gmail.com>
2022-12-15 18:34:33 +0100

    updated file

---
+++ src/stories/main.stories.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Icon from "../components/Icon";
+import * as Icons from "../index";
+
+
+
+export default { title: 'Components/Main' }; 
+
+
+/**
+ * @description - To test the icon component props 
+ * @key - color 
+ * @key - className
+ * @key - style
+ * @key - size
+ * 
+ * @retuns - 
+ * 
+ */
+
+export const Button = () => <Icons.BackIcon size={40} color="green"/>; 
+
+
+/**
+ * @description  - test for mobule system
+ * 
+ */
+
+
